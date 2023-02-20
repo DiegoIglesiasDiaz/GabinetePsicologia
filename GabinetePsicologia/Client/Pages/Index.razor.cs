67006372345d61b0a9adfc95112777ec27327072
Adrian Jimenez Sanz <ajimenezsanz@dimaticasoftware.com>
2023-02-20 08:44:38 +0100

    registro psicologo

---
+++ GabinetePsicologia/Client/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using GabinetePsicologia.Shared;
+using System.Net.Http.Json;
+
+namespace GabinetePsicologia.Client.Pages
+{
+    public partial class Index
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            var user = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User;
+            if (user != null && user.Identity.IsAuthenticated)
+            {
+                var pacientes = await http.GetFromJsonAsync<List<Paciente>>("Paciente");
+            }
+
+        }
+    }
+}
