599202ed544e5d9362238c59736415cc00dce9e2
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-04-12 23:41:01 +0200

    configuracion

---
+++ GabinetePsicologia/Client/Pages/Configuracion.razor.cs
@@ -0,0 +1,35 @@
+﻿using GabinetePsicologia.Shared;
+using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+using Radzen;
+using Microsoft.AspNetCore.Components.Authorization;
+using GabinetePsicologia.Client.Services;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GabinetePsicologia.Client.Pages
+{
+    public partial class Configuracion
+    {
+        [Inject] private NotificationService NotificationService { get; set; } 
+        [Inject] NavigationManager NavigationManager { get; set; }
+        [Inject] AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        [Inject] UsuarioServices UsuarioServices { get; set; }
+        public bool isInRole = false;
+        public string correo = "";
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var user = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User;
+            correo = user.Identity.Name;
+            if (user.IsInRole("Paciente") || user.IsInRole("Psicologo") || user.IsInRole("Administrador"))
+            {
+                isInRole = true;
+
+            } 
+        }
+        public void Logout()
+        {
+            NavigationManager.NavigateTo("/Logout", true);
+        }
+    }
+}
