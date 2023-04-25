1e16a2f4ce28fadb53770a6db5af43521e692e84
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-04-25 22:46:19 +0200

    cambio

--- GabinetePsicologia/Server/Program.cs
+++ GabinetePsicologia/Server/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddIdentityServer()
     .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(options => {
         options.IdentityResources["openid"].UserClaims.Add("role");
         options.ApiResources.Single().UserClaims.Add("role");
-    }).AddDeveloperSigningCredential();
+    });
 
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt()
