d84f7a332318720fe6da967c7a8b6f4017a48934
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-06-06 02:45:43 +0200

    creo trigger

--- GabinetePsicologia/Server/Data/procedures.sql
+++ GabinetePsicologia/Server/Data/procedures.sql
@@ -35,3 +35,21 @@ BEGIN
 	SELECT * from Psicologos ps inner join  AspNetUsers us on us.Id = ps.ApplicationUserId where us.Id = @id
 END
 
+Go
+
+CREATE TRIGGER eliminarMensaje_despues_nueve_meses
+ON Chat
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Calcula la fecha límite
+    DECLARE @fecha_limite AS DATE;
+    SET @fecha_limite = DATEADD(MONTH, -9, GETDATE());
+
+    -- Elimina las filas que cumplen la condición
+    DELETE FROM Chat
+    WHERE Date < @fecha_limite;
+END;
+GO
