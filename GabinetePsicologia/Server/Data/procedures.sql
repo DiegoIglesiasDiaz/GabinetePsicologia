9c5e5f7e5c36858bdf30a90d703407d10e139bfc
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-06-08 23:28:00 +0200

    pequeños cambios

--- GabinetePsicologia/Server/Data/procedures.sql
+++ GabinetePsicologia/Server/Data/procedures.sql
@@ -2,6 +2,8 @@
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+use GabinetePsicologia
+go
 
 CREATE PROCEDURE [dbo].[GetPacienteByUserName]
 @username varchar(40)
@@ -52,4 +54,14 @@ BEGIN
     DELETE FROM Chat
     WHERE Date < @fecha_limite;
 END;
+GO
+Create PROCEDURE [dbo].[GetPacienteById]
+@id uniqueidentifier
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	SELECT * from Pacientes ps where ps.Id = @id
+END
 GO
