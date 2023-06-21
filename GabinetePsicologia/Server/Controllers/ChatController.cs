883d423291d5742413c3ad1adc78b01aba586ccb
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-06-21 23:19:54 +0200

    s

--- GabinetePsicologia/Server/Controllers/ChatController.cs
+++ GabinetePsicologia/Server/Controllers/ChatController.cs
@@ -17,7 +17,7 @@ using Cita = GabinetePsicologia.Shared.Cita;
 
 namespace GabinetePsicologia.Server.Controllers
 {
-    [Authorize(Roles = "AppUsers")]
+    [Authorize(Roles = "Administrador")]
     [Route("[controller]")]
 	[ApiController]
 
