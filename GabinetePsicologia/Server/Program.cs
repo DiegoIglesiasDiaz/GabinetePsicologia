28409f6c72153c824db0e04baf9d707a67ded764
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-06-21 23:29:43 +0200

    s

--- GabinetePsicologia/Server/Program.cs
+++ GabinetePsicologia/Server/Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddIdentityServer()
     });
 
 //sigue dando 401 al llamar la api intentar poner ValidateIssuer false;
-builder.Services.AddAuthentication()
+builder.Services.AddAuthentication().AddIdentityServerJwt()
     .AddGoogle(googleOptions =>
     {
         googleOptions.ClientId = builder.Configuration["GoogleClientId"]!;
