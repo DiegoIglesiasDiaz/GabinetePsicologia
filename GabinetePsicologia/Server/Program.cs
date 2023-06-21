e988278dacf04b41baae8c83a407c378d41fa8e3
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-06-21 21:52:57 +0200

    cambio

--- GabinetePsicologia/Server/Program.cs
+++ GabinetePsicologia/Server/Program.cs
@@ -43,13 +43,13 @@ builder.Services.AddAuthentication().AddJwtBearer(options =>
 {
 	options.TokenValidationParameters = new TokenValidationParameters
 	{
-		ValidateIssuer = true,
-		ValidateAudience = true,
-		ValidateLifetime = true,
-		ValidateIssuerSigningKey = true,
-		ValidIssuer = "https://app.diegoiglesiasdiaz.com",
-		ValidAudience = "your_audience",
-		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("7DMmGbe11rjZWvmY2pr6wLdEZAgqvcYo"))
+		ValidateIssuer = false,
+		//ValidateAudience = true,
+		//ValidateLifetime = true,
+		//ValidateIssuerSigningKey = true,
+		//ValidIssuer = "https://app.diegoiglesiasdiaz.com",
+		//ValidAudience = "your_audience",
+		//IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("7DMmGbe11rjZWvmY2pr6wLdEZAgqvcYo"))
 	};
 }).AddIdentityServerJwt()
     .AddGoogle(googleOptions =>
