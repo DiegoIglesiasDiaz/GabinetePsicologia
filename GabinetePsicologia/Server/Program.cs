681932498baf15720fdba80fe4e5a265ace906ec
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2023-06-21 23:57:51 +0200

    s

--- GabinetePsicologia/Server/Program.cs
+++ GabinetePsicologia/Server/Program.cs
@@ -47,14 +47,16 @@ builder.Services.AddAuthentication().AddJwtBearer(options =>
     var validIssuers = new string[] { "https://diegoiglesiasdiaz.com", "https://centrodetecnicasnaturalesneo.com/" };
     options.TokenValidationParameters = new TokenValidationParameters
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuers = validIssuers,
-        ValidIssuer = "https://centrodetecnicasnaturalesneo.com/",
-        ValidAudience = "your_audience",
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("7DMmGbe11rjZWvmY2pr6wLdEZAgqvcYo"))
+        ValidateIssuer = false,
+        ValidateActor = false,
+        ValidateTokenReplay = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = false,
+        //ValidIssuers = validIssuers,
+        //ValidIssuer = "https://centrodetecnicasnaturalesneo.com/",
+        //ValidAudience = "your_audience",
+        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("7DMmGbe11rjZWvmY2pr6wLdEZAgqvcYo"))
     };
 }).AddIdentityServerJwt()
 .AddGoogle(googleOptions =>
