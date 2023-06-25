3d5f60b3e223a8ca473e822eca366551e44fffc5
Diego Iglesias Diaz <diglesiasdiaz@dimaticasoftware.com>
2023-06-25 19:06:31 +0200

    cx

--- GabinetePsicologia/Server/Models/AuthAttribute.cs
+++ GabinetePsicologia/Server/Models/AuthAttribute.cs
@@ -4,29 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Security.Claims;
 using System.Web.Http.Controllers;
-using System.Web.Http.Filters;
 
 namespace GabinetePsicologia.Server.Models
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
-    public sealed class AuthAttribute : AuthorizationFilterAttribute
+    public sealed class AuthAttribute : Attribute, IAuthorizationFilter
     {
-        public override void OnAuthorization(HttpActionContext context)
+        public void OnAuthorization(AuthorizationFilterContext context)
         {
-            context.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
-            //if (context != null)
-            //{
-            //    if (!context.RequestContext.Principal.Identity.IsAuthenticated)
-            //    {
-            //        context.Result = new ForbidResult();
+            if (context != null)
+            {
+                if (!context.HttpContext.User.IsAuthenticated())
+                {
+                    context.Result = new ForbidResult();
 
-            //    }
+                }
 
-            //}
-            //else
-            //{
-            //    context.Result = new ForbidResult();
-            //}
+            }
+            else
+            {
+                context.Result = new ForbidResult();
+            }
         }
     }
 }
